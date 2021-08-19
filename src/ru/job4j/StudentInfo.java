fbf749a737f73d808970840ed54ddd3a0ea6ab49
veiuper <valeriyfinans@gmail.com>
2021-08-20 01:02:59 +0500

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Valerii Osipov");
+        System.out.println("21.05.1984");
+
+    }
+}
