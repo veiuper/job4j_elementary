3dcf823498dd01d72d1ca108f2508baf2282e000
veiuper <valeriyfinans@gmail.com>
2021-08-24 10:25:45 +0500

    9.2. Checkstyle [#4123 #193528]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Main {
 
-    public static void main(String... args) {
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
