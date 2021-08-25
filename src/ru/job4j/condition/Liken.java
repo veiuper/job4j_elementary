fe0770748573171b1e8f92a554b5c00a71fa2981
veiuper <valeriyfinans@gmail.com>
2021-08-25 23:56:35 +0500

    0. Операции сравнения [#235631]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Liken {
+
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean rsl = first > second;
+        System.out.println(rsl);
+
+        rsl = first < second;
+        System.out.println(rsl);
+
+        rsl = first == second;
+        System.out.println(rsl);
+    }
+}
