c0c0d2b8d0c1178b4dc266dc95e378939394c32e
veiuper <valeriyfinans@gmail.com>
2021-08-26 15:19:05 +0500

    4.3.5. Множественное логическое выражение И [#9461]

---
+++ test/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+
+    @Test
+    public void whenExist() {
+        double in1 = 1.5;
+        double in2 = 2.0;
+        double in3 = 3.0;
+        boolean actual = Triangle.exist(in1, in2, in3);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void whenNotExist() {
+        double in1 = 1.0;
+        double in2 = 2.0;
+        double in3 = 3.0;
+        boolean actual = Triangle.exist(in1, in2, in3);
+        Assert.assertFalse(actual);
+    }
+}
