ffa262551e1c3f3218588deb243bda79f5423107
veiuper <valeriyfinans@gmail.com>
2021-08-26 15:02:56 +0500

    4.3.4. if c return [#333785]

---
+++ src/ru/job4j/condition/PointEq.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class PointEq {
+
+    public static boolean eq(int x1, int y1, int x2, int y2) {
+        return x1 == x2 && y1 == y2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PointEq.eq(5, 2, 3, -2));
+        System.out.println(PointEq.eq(5, -2, 5, -2));
+    }
+}
