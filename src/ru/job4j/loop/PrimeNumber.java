dceab29b28e8cd24c402d5eb1c6336a283de4c5a
veiuper <valeriyfinans@gmail.com>
2021-08-27 23:46:00 +0500

    5.5. Простое число [#171694]

---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public static int calc(int finish) {
+
+        int count = 0;
+
+        for (int i = 2; i <= finish; i++) {
+            if (CheckPrimeNumber.check(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ test/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PrimeNumberTest {
+
+    @Test
+    public void when5() {
+        int in = 5;
+        int expected = 3;
+        int actual = PrimeNumber.calc(in);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void when11() {
+        int in = 11;
+        int expected = 5;
+        int actual = PrimeNumber.calc(in);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void when2() {
+        int in = 2;
+        int expected = 1;
+        int actual = PrimeNumber.calc(in);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void when1() {
+        int in = 1;
+        int expected = 0;
+        int actual = PrimeNumber.calc(in);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenMinus1() {
+        int in = -1;
+        int expected = 0;
+        int actual = PrimeNumber.calc(in);
+        Assert.assertEquals(expected, actual);
+    }
+}
